763e3341d7110e0fc001c46bba7739a9af068d08
Matt Mitchell <mmitche@microsoft.com>
2017-03-08 09:49:58 -0800

    Implement tools and docker initialization

--- portable-linux-innerloop.groovy
+++ portable-linux-innerloop.groovy
@@ -1,7 +1,15 @@
+// Todo: Refactor library so that we could have:
+// standardNode('Ubuntu 16.04', 'latest') {
+//     ...
+// }
+// ... is wrapped by a try/finally that defines workspace cleanup
 node('ubuntu1604-20170307') {
     try {
-        checkout scm
-        sh 'ls'
+        checkout scm // Check out source control (based on parent scm settings)
+        // Initialize tools
+        sh './init-tools.sh'
+        // Initialize docker
+        sh './Tools/scripts/docker/init-docker.sh'
     }
     finally {
         step([$class: 'WsCleanup'])        
