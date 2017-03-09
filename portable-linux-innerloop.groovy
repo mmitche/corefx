ee3b65661408b80e6a0f8548c8cf1606e4b8d2b6
Matt Mitchell <mmitche@microsoft.com>
2017-03-09 10:41:43 -0800

    Add basic docker functionality

--- portable-linux-innerloop.groovy
+++ portable-linux-innerloop.groovy
@@ -3,17 +3,33 @@
 //     ...
 // }
 // ... is wrapped by a try/finally that defines workspace cleanup
+
+// Define some top level parameters
 stage ('Build') {
     node('ubuntu1604-20170216') {
+        def dockerContainerName = BUILD_TAG
+        def dockerRepository = 'microsoft/dotnet-buildtools-prereqs'
+        def dockerTag = 'rhel7_prereqs_2'
+        def dockerImageName = "${dockerRepository}:${dockerTag}"
+        def hostWorkspaceDir = pwd()
+        def dockerWorkspaceDir = '/root/workspace'
         try {
             checkout scm // Check out source control (based on parent scm settings)
             // Initialize tools
-            sh './init-tools.sh'
+            sh "./init-tools.sh"
             // Initialize docker
-            sh './Tools/scripts/docker/init-docker.sh'
+            sh "./Tools/scripts/docker/init-docker.sh ${dockerImageName}"
+            // Below might be great to wrap:
+            // withDocker (dockerImageName) { 
+            // Start docker, expose the workspace directory to the docker container
+            sh "docker run -d -v ${workspaceDir}:${dockerWorkspaceDir} --name ${dockerContainerName} ${dockerImageName} sleep 7200"
+            // Generate the version assets
+            sh "docker exec ${dockerContainerName} ${dockerWorkspaceDir}/build-managed.sh -OfficialBuildId=${ghprbActualCommit} -- /t:GenerateVersionSourceFile /p:GenerateVersionSourceFile=true"
         }
         finally {
-            step([$class: 'WsCleanup'])        
+            step([$class: 'WsCleanup'])
+            sh "docker stop ${dockerContainerName}"
+            sh "docker rm ${dockerContainerName}"
         }
     }
 }
