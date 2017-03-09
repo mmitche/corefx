c760210da1d3e64e0aa4d17877bd15fd5494c5b6
Matt Mitchell <mmitche@microsoft.com>
2017-03-09 09:40:05 -0800

    Add stage

--- portable-linux-innerloop.groovy
+++ portable-linux-innerloop.groovy
@@ -3,15 +3,17 @@
 //     ...
 // }
 // ... is wrapped by a try/finally that defines workspace cleanup
-node('ubuntu1604-20170216') {
-    try {
-        checkout scm // Check out source control (based on parent scm settings)
-        // Initialize tools
-        sh './init-tools.sh'
-        // Initialize docker
-        sh './Tools/scripts/docker/init-docker.sh'
-    }
-    finally {
-        step([$class: 'WsCleanup'])        
+stage ('Build') {
+    node('ubuntu1604-20170216') {
+        try {
+            checkout scm // Check out source control (based on parent scm settings)
+            // Initialize tools
+            sh './init-tools.sh'
+            // Initialize docker
+            sh './Tools/scripts/docker/init-docker.sh'
+        }
+        finally {
+            step([$class: 'WsCleanup'])        
+        }
     }
 }
