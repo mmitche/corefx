15f86a35704ed01c865006aab05cc1a6f1bd57fb
Matt Mitchell <mmitche@microsoft.com>
2017-03-09 10:49:45 -0800

    Wrap in stages, rework cleanup stages

--- portable-linux-innerloop.groovy
+++ portable-linux-innerloop.groovy
@@ -4,32 +4,41 @@
 // }
 // ... is wrapped by a try/finally that defines workspace cleanup
 
-// Define some top level parameters
-stage ('Build') {
-    node('ubuntu1604-20170216') {
-        def dockerContainerName = BUILD_TAG
-        def dockerRepository = 'microsoft/dotnet-buildtools-prereqs'
-        def dockerTag = 'rhel7_prereqs_2'
-        def dockerImageName = "${dockerRepository}:${dockerTag}"
-        def hostWorkspaceDir = pwd()
-        def dockerWorkspaceDir = '/root/workspace'
-        try {
+node('ubuntu1604-20170216') {
+    def dockerContainerName = BUILD_TAG
+    def dockerRepository = 'microsoft/dotnet-buildtools-prereqs'
+    def dockerTag = 'rhel7_prereqs_2'
+    def dockerImageName = "${dockerRepository}:${dockerTag}"
+    def hostWorkspaceDir = pwd()
+    def dockerWorkspaceDir = '/root/workspace'
+    try {
+        stage ('Checkout Source') {
             checkout scm // Check out source control (based on parent scm settings)
+        }
+        stage ("Initialize tools and docker") {
             // Initialize tools
             sh "./init-tools.sh"
             // Initialize docker
             sh "./Tools/scripts/docker/init-docker.sh ${dockerImageName}"
+        }
+        stage ("Start docker (Name: ${dockerContainerName}, Image: ${dockerImageName})") {
             // Below might be great to wrap:
             // withDocker (dockerImageName) { 
             // Start docker, expose the workspace directory to the docker container
             sh "docker run -d -v ${workspaceDir}:${dockerWorkspaceDir} --name ${dockerContainerName} ${dockerImageName} sleep 7200"
+        }
+        stage ("Build corefx") {
             // Generate the version assets
             sh "docker exec ${dockerContainerName} ${dockerWorkspaceDir}/build-managed.sh -OfficialBuildId=${ghprbActualCommit} -- /t:GenerateVersionSourceFile /p:GenerateVersionSourceFile=true"
         }
-        finally {
-            step([$class: 'WsCleanup'])
+    }
+    finally {
+        stage ("Clean-up docker") {
             sh "docker stop ${dockerContainerName}"
             sh "docker rm ${dockerContainerName}"
+        }
+        stage ("Clean-up workspace") {
+            step([$class: 'WsCleanup'])
         }
     }
 }
