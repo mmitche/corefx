2e52f704686f7107372fa44c96d20b01a1972733
Matt Mitchell <mmitche@microsoft.com>
2017-03-14 10:52:23 -0700

    Add parrallel wait

--- portable-linux-innerloop.groovy
+++ portable-linux-innerloop.groovy
@@ -8,7 +8,7 @@ def configuration = "Release";
 def dockerRepository = 'microsoft/dotnet-buildtools-prereqs'
 def dockerTag = 'rhel7_prereqs_2'
 def dockerImageName = "${dockerRepository}:${dockerTag}"
-def targetHelixQueues = 'Redhat.72.Amd64'
+def targetHelixQueues = 'Redhat.72.Amd64,'
 
 node('ubuntu1604-20170216') {
     try {
@@ -38,11 +38,35 @@ node('ubuntu1604-20170216') {
                 withCredentials([string(credentialsId: 'CloudDropAccessToken', variable: 'CloudDropAccessToken'),
                                  string(credentialsId: 'OutputCloudResultsAccessToken', variable: 'OutputCloudResultsAccessToken'),
                                  string(credentialsId: 'HelixApiAccessKey', variable: 'HelixApiAccessKey')]) {
-                    sh "./Tools/msbuild.sh src/upload-tests.proj /p:ArchGroup=x64 /p:ConfigurationGroup=${configuration} /p:EnableCloudTest=true /p:TestProduct=corefx /p:TimeoutInSeconds=1200 /p:TargetOS=Linux /p:CloudDropAccountName=dotnetbuilddrops /p:CloudResultsAccountName=dotnetjobresults /p:CloudDropAccessToken=\$CloudDropAccessToken /p:CloudResultsAccessToken=\$OutputCloudResultsAccessToken /p:HelixApiAccessKey=\$HelixApiAccessKey /p:HelixApiEndpoint=https://helix.dot.net/api/2016-06-28/jobs /p:Branch=${ghprbPullId} /p:TargetQueues=Redhat.72.Amd64 /p:HelixLogFolder=${WORKSPACE}/bin/ /p:HelixCorrelationInfoFileName=SubmittedHelixRuns.txt"
+                    sh "./Tools/msbuild.sh src/upload-tests.proj /p:ArchGroup=x64 /p:ConfigurationGroup=${configuration} /p:EnableCloudTest=true /p:TestProduct=corefx /p:TimeoutInSeconds=1200 /p:TargetOS=Linux /p:CloudDropAccountName=dotnetbuilddrops /p:CloudResultsAccountName=dotnetjobresults /p:CloudDropAccessToken=\$CloudDropAccessToken /p:CloudResultsAccessToken=\$OutputCloudResultsAccessToken /p:HelixApiAccessKey=\$HelixApiAccessKey /p:HelixApiEndpoint=https://helix.dot.net/api/2016-06-28/jobs /p:Branch=${ghprbPullId} /p:TargetQueues=${targetHelixQueues} /p:HelixLogFolder=${WORKSPACE}/bin/ /p:HelixCorrelationInfoFileName=SubmittedHelixRuns.txt"
                 }
 
-                // Grab the output info
-                sh "cat bin/SubmittedHelixRuns.txt"
+                // This moves into shared code.
+                // Something like:
+                // waitForHelixRuns(fileContainingRuns)
+                // Read the json in and spawn off to wait for these to finish
+                def helixRuns = readJSON file: 'bin/SubmittedHelixRuns.txt'
+                // Is an array of:
+                // {
+                //      CorrelationId
+                //      QueueId
+                //      QueueTimeUtc
+                // }
+                def waitForHelixRuns = [:]
+                for (int i = 0; i < helixRuns.size(); i++) {
+                    def currentRun = helixRuns[i];
+                    def queueId = currentRun['QueueId']
+                    def correlationId = currentRun['CorrelationId']
+                    waitForHelixRuns[currentRun['QueueId']] = {
+                        waitUntil {
+                            echo "Running tests on ${queueId} (${correlationId})"
+                            return true;
+                        }
+                    }
+                }
+            }
+            stage ('Execute Tests') {
+                parallel waitForHelixRuns
             }
         }
     }
