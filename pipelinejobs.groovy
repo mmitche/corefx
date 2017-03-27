1edaf2cdc6722eb9f5e133c5bd01416b3b04b79c
Matt Mitchell <mmitche@microsoft.com>
2017-03-27 15:05:49 -0700

    Add pipeline jobs definitions

---
+++ pipelinejobs.groovy
@@ -0,0 +1,25 @@
+// Import the utility functionality.
+
+import jobs.generation.JobReport;
+import src.org.dotnet.ci.pipelines.Pipeline
+
+// The input project name (e.g. dotnet/corefx)
+def project = GithubProject
+// The input branch name (e.g. master)
+def branch = GithubBranchName
+
+// **************************
+// Define innerloop testing.  These jobs run on every merge and a subset of them run on every PR, the ones
+// that don't run per PR can be requested via a magic phrase.
+// **************************
+newPipeline = Pipelines.createPipelineForGithub(this, project, branch, 'portable-linux-innerloop.groovy')
+
+['netcoreapp'].each { targetGroup ->
+	['Debug', 'Release'].each { configurationGroup ->
+		['PortableLinux'].each { osName ->
+			newPipeline.triggerPipelineOnEveryGithubPR("${osName} ${configurationGroup}", ['Configuration', configurationGroup])
+		}
+	}
+}
+
+JobReport.Report.generateJobReport(out)
