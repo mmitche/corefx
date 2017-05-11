f72e4c8c79564c864708831e81190e83b5ed8fd6
Matt Mitchell <mmitche@microsoft.com>
2017-05-10 18:19:32 -0700

    Attempt to separate stages

--- portable-windows.groovy
+++ portable-windows.groovy
@@ -5,6 +5,8 @@
 //          in the build scripts and this can cause problems.
 // Outerloop - If true, runs outerloop, if false runs just innerloop
 
+def submittedHelixJson = null
+
 simpleNode('Windows_NT','latest') {
     stage ('Checkout source') {
         checkout scm
@@ -46,11 +48,12 @@ simpleNode('Windows_NT','latest') {
             def targetHelixQueues = 'Windows.10.Amd64.Open,Windows.7.Amd64.Open,Windows.81.Amd64.Open'
 
             bat "\"%VS140COMNTOOLS%\\VsDevCmd.bat\" && msbuild src\\upload-tests.proj /p:ArchGroup=x64 /p:ConfigurationGroup=${Config} /p:TestProduct=corefx /p:TimeoutInSeconds=1200 /p:TargetOS=Windows_NT /p:HelixJobType=test/functional/portable/cli/ /p:HelixSource=${helixSource} /p:Build=${helixBuild} /p:HelixCreator=${helixCreator} /p:CloudDropAccountName=dotnetbuilddrops /p:CloudResultsAccountName=dotnetjobresults /p:CloudDropAccessToken=%CloudDropAccessToken% /p:CloudResultsAccessToken=%OutputCloudResultsAccessToken% /p:HelixApiEndpoint=https://helix.dot.net/api/2017-04-14/jobs /p:TargetQueues=\"${targetHelixQueues}\" /p:HelixLogFolder=${logFolder} /p:HelixCorrelationInfoFileName=SubmittedHelixRuns.txt"
+
+            submittedHelixJson = readJSON file: "${logFolder}/SubmittedHelixRuns.txt"
         }
     }
+}
 
-    stage ('Execute Tests') {
-        def submittedHelixJson = readJSON file: "${logFolder}/SubmittedHelixRuns.txt"
-        waitForHelixRuns(submittedHelixJson, "Windows x64 Tests - ${Config}")
-    }
+stage ('Execute Tests') {
+    waitForHelixRuns(submittedHelixJson, "Windows x64 Tests - ${Config}")
 }
